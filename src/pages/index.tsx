031562945a1d2ff91cee4c9a6b7ae1e07bae1452
davulrich <david.ulrich@merck.com>
2022-08-02 00:03:28 +0200

    Add typography adn registr font-families

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,19 +1,11 @@
+import {Button, Typography} from '@mui/material'
 import type {NextPage} from 'next'
-import Slider from '@mui/material/Slider'
-import {styled} from '@mui/material/styles'
-
-const CustomizedSlider = styled(Slider)`
-  color: #20b2aa;
-
-  :hover {
-    color: #2e8b57;
-  }
-`
 
 const Home: NextPage = () => {
   return (
     <div>
-      <CustomizedSlider />
+      <Button>Test</Button>
+      <Typography variant="h1">Title</Typography>
     </div>
   )
 }
---
+++ src/styles/Typography.ts
@@ -0,0 +1,70 @@
+import {createGlobalStyle} from 'styled-components'
+
+const styled = {createGlobalStyle}
+
+const Typography = styled.createGlobalStyle`
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamLight.woff2') format('woff2');
+    font-weight: 200;
+    font-style: normal;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamLightItalic.woff2') format('woff2');
+    font-weight: 200;
+    font-style: italic;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamBook.woff2') format('woff2');
+    font-weight: 400;
+    font-style: normal;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamBookItalic.woff2') format('woff2');
+    font-weight: 400;
+    font-style: italic;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamMedium.woff2') format('woff2');
+    font-weight: 500;
+    font-style: normal;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamMediumItalic.woff2') format('woff2');
+    font-weight: 500;
+    font-style: italic;
+    font-display: fallback;
+  }
+
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamBold.woff2') format('woff2');
+    font-weight: 700;
+    font-style: normal;
+    font-display: fallback;
+  }
+  @font-face {
+    font-family: 'Gotham';
+    src: url('/fonts/GothamBoldItalic.woff2') format('woff2');
+    font-weight: 700;
+    font-style: italic;
+    font-display: fallback;
+  }
+`
+
+export default Typography
