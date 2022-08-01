e152f2abe497f58b76f476e4d8c50e397ddff1a5
davulrich <david.ulrich@merck.com>
2022-08-01 22:07:56 +0200

    Add MUI library with styled components

--- next.config.js
+++ next.config.js
@@ -1,10 +1,20 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
+const withTM = require('next-transpile-modules')([
+  '@mui/material',
+  '@mui/system',
+  //'@mui/icons-material', // If @mui/icons-material is being used
+])
+
+module.exports = withTM({
   reactStrictMode: true,
   swcMinify: true,
   compiler: {
     styledComponents: true,
   },
-}
-
-module.exports = nextConfig
+  webpack: (config) => {
+    config.resolve.alias = {
+      ...config.resolve.alias,
+      '@mui/styled-engine': '@mui/styled-engine-sc',
+    }
+    return config
+  },
+})
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,9 +1,19 @@
 import type {NextPage} from 'next'
+import Slider from '@mui/material/Slider'
+import {styled} from '@mui/material/styles'
+
+const CustomizedSlider = styled(Slider)`
+  color: #20b2aa;
+
+  :hover {
+    color: #2e8b57;
+  }
+`
 
 const Home: NextPage = () => {
   return (
     <div>
-      <h1>Home</h1>
+      <CustomizedSlider />
     </div>
   )
 }
